a1e2e06b3678f81573ab160968b801918347ea06
zrxk <kimraid@gmail.com>
2024-08-28 19:34:16 +0800

    Update diy-part1.sh

--- config/Lienol-master/diy-part1.sh
+++ config/Lienol-master/diy-part1.sh
@@ -7,7 +7,7 @@
 #========================================================================================================================
 
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
