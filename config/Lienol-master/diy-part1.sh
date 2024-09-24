b7ef82aa9d62ce2f36e15eabd4b4f89453778047
zrxk <kimraid@gmail.com>
2024-09-24 16:16:39 +0800

    Update diy-part1.sh

--- config/Lienol-master/diy-part1.sh
+++ config/Lienol-master/diy-part1.sh
@@ -10,7 +10,7 @@
 sed -i '$a src-git zrxk https://github.com/zrxk/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;neko' feeds.conf.default
+sed -i '$a src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main' feeds.conf.default
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
